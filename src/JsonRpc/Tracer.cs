a6b06930f52cf4459b74cc44169e0ec0a2aa14cf
Martin Lercher <Lercher@singhammer.de>
2017-05-06 15:13:13 +0200

    Tracer with timing

--- src/JsonRpc/Tracer.cs
+++ src/JsonRpc/Tracer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 
 namespace JsonRpc
@@ -6,6 +7,7 @@ namespace JsonRpc
     public static class Tracer
     {
         private static TextWriter tw = null;
+        private static Stopwatch sw;
 
         public static void Connect(Stream channel)
         {
@@ -15,12 +17,16 @@ namespace JsonRpc
         public static void Connect(TextWriter writer)
         {
             tw = writer;
+            sw = Stopwatch.StartNew();
         }
 
         public static void Do(Action<TextWriter> writeLineAction)
         {
             if (tw != null && writeLineAction != null)
+            {
+                tw.Write(sw.Elapsed.ToString());
                 writeLineAction.Invoke(tw);
+            }                
         }
     }
 }
