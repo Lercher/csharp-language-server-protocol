1bcdf414c89d5c84a6e829756be8dacb1d44bf91
Martin Lercher <lercher@singhammer.de>
2017-05-12 17:03:21 +0200

    Supporting multiple documents.

---
+++ sample/SampleServer/ParseUnit.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lsp;
+using Lsp.Models;
+using Lsp.Protocol;
+using SampleServer.WFModel;
+
+namespace SampleServer
+{
+    class ParseUnit
+    {
+        public Uri file { get; }
+        public WFModel.Parser parser { get; private set; }
+        private readonly System.Text.StringBuilder sourcecode = new System.Text.StringBuilder();
+        private readonly ILanguageServer _router;
+
+
+        public ParseUnit(ILanguageServer ls, Uri file)
+        {
+            _router = ls;
+            this.file = file;
+        }
+
+        private static IEnumerable<string> LinesOf(System.Text.StringBuilder sb)
+        {
+            using (var sr = new System.IO.StringReader(sb.ToString()))
+                while (true)
+                {
+                    var line = sr.ReadLine();
+                    if (line == null)
+                        break;
+                    yield return line;
+                }
+        }
+
+        private static long PosOf(long LineNumber, string[] Lines, int WithLineEndingSize)
+        {
+            var pos = 0L;
+            for (var i = 0; i < LineNumber && i < Lines.Length; i++)
+                pos += (Lines[i].Length + WithLineEndingSize);
+            return pos;
+        }
+
+        private static long PosOf(Position p, string[] lines)
+        {
+            if (p == null)
+                return 0L;
+            return p.Character + PosOf(p.Line, lines, 2);
+        }
+
+        public void ApplyChanges(string fullText)
+        {
+            sourcecode.Clear();
+            sourcecode.Append(fullText);
+            Parse();
+        }
+
+        public void ApplyChanges(Container<TextDocumentContentChangeEvent> contentChanges)
+        {
+            if (contentChanges == null || !contentChanges.Any())
+                return;
+
+            // special case: a single contentChange item with no Range contains the full source
+            if (contentChanges.Count() == 1 && contentChanges.First().Range == null)
+            {
+                ApplyChanges(contentChanges.First().Text);
+                return;
+            }
+
+            var lines = LinesOf(sourcecode).ToArray();
+            var qy =
+                from item in contentChanges
+                select new
+                {
+                    item.Text,
+                    Start = (int)PosOf(item.Range?.Start, lines),
+                    End = (int)PosOf(item.Range?.End, lines)
+                };
+            var qy2 =
+                from item in qy
+                orderby item.Start descending
+                select item;
+
+            foreach (var c in qy2)
+            {
+                sourcecode.Remove(c.Start, c.End - c.Start);
+                sourcecode.Insert(c.Start, c.Text);
+            }
+            Parse();
+        }
+
+        private void Parse()
+        {
+            lock (this)
+            {
+                var b = System.Text.Encoding.UTF8.GetBytes(sourcecode.ToString());
+                var sb = new System.Text.StringBuilder();
+
+                using (var w = new System.IO.StringWriter(sb))
+                using (var s = new System.IO.MemoryStream(b))
+                {
+                    var scanner = new WFModel.Scanner(s, true); // it's BOM free but UTF8
+                    parser = new WFModel.Parser(scanner);
+                    parser.errors.errorStream = w;
+                    parser.Parse();
+                    w.WriteLine("\n{0:n0} error(s) detected", parser.errors.count);
+                }
+                _router.LogMessage(sb.ToString());
+                foreach (var a in parser.tokens.Take(50))
+                    foreach (var s in a.Describe())
+                        _router.LogMessage(s);
+            }
+        }
+
+        public Hover CreateHover(Alternative a)
+        {
+            if (a == null) return CreateHover(string.Empty);
+            return CreateHover(a.Describe(), a.t.ToRange());
+        }
+
+        public Hover CreateHover(string s)
+        {
+            return new Hover() { Contents = new MarkedStringContainer(new MarkedString(s)) };
+        }
+
+        private Hover CreateHover(IEnumerable<string> ss, Range r)
+        {
+            // see http://vshaxe.github.io/vscode-extern/vscode/MarkedString.html
+            _router.LogMessage("from " + r.Start.Describe() + "-" + r.End.Describe());
+            var qy = from s in ss select new MarkedString(s); // "markdown"
+            return new Hover() { Contents = new MarkedStringContainer(qy.ToArray()), Range = r };
+        }
+
+    }
+}
--- sample/SampleServer/Program.cs
+++ sample/SampleServer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -12,7 +13,7 @@ using Lsp.Protocol;
 using SampleServer.WFModel;
 
 namespace SampleServer
-{    
+{
     class Program
     {
         static void Main(string[] args)
@@ -28,7 +29,7 @@ namespace SampleServer
             //}
 
             Tracer.Connect(Console.Error);
-            Tracer.Deactivate(new int[] {1, 2});
+            Tracer.Deactivate(new int[] { 1, 2 });
             var server = new LanguageServer(Console.OpenStandardInput(), Console.OpenStandardOutput());
 
             server.AddHandler(new TextDocumentHandler(server));
@@ -44,12 +45,11 @@ namespace SampleServer
     class TextDocumentHandler : ITextDocumentSyncHandler, IHoverHandler
     {
         private readonly ILanguageServer _router;
-        private string sourcecode;
-        private WFModel.Scanner scanner;
-        private WFModel.Parser parser;
+        private readonly ConcurrentDictionary<Uri, ParseUnit> _ParseUnits;
 
         private readonly DocumentSelector _documentSelector = new DocumentSelector(
-            new DocumentFilter() {
+            new DocumentFilter()
+            {
                 Pattern = "**/*",
                 Language = "plaintext"
                 //Pattern = "**/*.csproj",
@@ -63,96 +63,18 @@ namespace SampleServer
         public TextDocumentHandler(ILanguageServer router)
         {
             _router = router;
+            _ParseUnits = new ConcurrentDictionary<Uri, ParseUnit>();
         }
 
-        private static IEnumerable<string> LinesOf(string s)
+
+        public TextDocumentSyncOptions Options { get; } = new TextDocumentSyncOptions()
         {
-            using (var sr = new System.IO.StringReader(s))
-                while(true)
-                {
-                    var line = sr.ReadLine();
-                    if (line == null)
-                        break;
-                    yield return line;
-                }
-        }
-
-        private static long PosOf(long LineNumber, string[] Lines, int WithLineEndingSize)
-        {
-            var pos = 0L;
-            for (var i = 0; i < LineNumber && i < Lines.Length; i++)
-                pos += (Lines[i].Length + WithLineEndingSize);
-            return pos;
-        }
-
-        private static long PosOf(Position p, string[] lines)
-        {
-            if (p == null)
-                return 0L;
-            return p.Character + PosOf(p.Line, lines, 2);
-        }
-
-        private static string ApplyChanges(string sourcecode, Container<TextDocumentContentChangeEvent> contentChanges)
-        {
-            if (contentChanges == null || !contentChanges.Any())
-                return sourcecode;
-
-            // special case: a single contentChange item with no Range contains the full source
-            if (contentChanges.Count() == 1 && contentChanges.First().Range == null)
-                return contentChanges.First().Text;
-
-            var lines = LinesOf(sourcecode).ToArray();
-            var qy =
-                from item in contentChanges
-                select new { 
-                    item.Text, 
-                    Start = (int)PosOf(item.Range?.Start, lines),
-                    End = (int)PosOf(item.Range?.End, lines)
-                };
-            var qy2 = 
-                from item in qy
-                orderby item.Start descending
-                select item;
-
-            var sb = new System.Text.StringBuilder(sourcecode);
-            foreach (var c in qy2)
-            {
-                sb.Remove(c.Start, c.End - c.Start);
-                sb.Insert(c.Start, c.Text);
-            }
-            return sb.ToString();
-        }
-
-        void Parse(string text)
-        {
-            lock (this)
-            {
-                sourcecode = text;
-                var b = System.Text.Encoding.UTF8.GetBytes(sourcecode);
-                var sb = new System.Text.StringBuilder();
-
-                using (var w = new System.IO.StringWriter(sb))
-                using (var s = new System.IO.MemoryStream(b))
-                {
-                    scanner = new WFModel.Scanner(s, true); // it's BOM free but UTF8
-                    parser = new WFModel.Parser(scanner);
-                    parser.errors.errorStream = w;
-                    parser.Parse();
-                    w.WriteLine("\n{0:n0} error(s) detected", parser.errors.count);
-                }
-                _router.LogMessage(sb.ToString());
-                foreach(var a in parser.tokens.Take(50))
-                    foreach(var s in a.Describe())
-                        _router.LogMessage(s);
-            }
-        }
-
-        public TextDocumentSyncOptions Options { get; } = new TextDocumentSyncOptions() {
             WillSaveWaitUntil = false,
             WillSave = true,
             Change = TextDocumentSyncKind.Full,
-            Save = new SaveOptions() {
-                IncludeText = true
+            Save = new SaveOptions()
+            {
+                IncludeText = true // TODO: try false
             },
             OpenClose = true
         };
@@ -160,14 +82,19 @@ namespace SampleServer
         public async Task Handle(DidChangeTextDocumentParams notification)
         {
             _router.LogMessage("DidChangeTextDocumentParams");
-            var newsource = ApplyChanges(sourcecode, notification.ContentChanges);
-            Parse(newsource);
+            ParseUnitFor(notification.TextDocument).ApplyChanges(notification.ContentChanges);
             await Task.CompletedTask;
+        }
+
+        private ParseUnit ParseUnitFor(TextDocumentIdentifier tdi)
+        {
+            return _ParseUnits.GetOrAdd(tdi.Uri, u => new ParseUnit(_router, u));
         }
 
         TextDocumentChangeRegistrationOptions IRegistration<TextDocumentChangeRegistrationOptions>.GetRegistrationOptions()
         {
-            return new TextDocumentChangeRegistrationOptions() {
+            return new TextDocumentChangeRegistrationOptions()
+            {
                 DocumentSelector = _documentSelector,
                 SyncKind = Options.Change
             };
@@ -181,13 +108,14 @@ namespace SampleServer
         public async Task Handle(DidOpenTextDocumentParams notification)
         {
             _router.LogMessage("DidOpenTextDocumentParams ");
-            Parse(notification.TextDocument.Text);
+            ParseUnitFor(notification.TextDocument).ApplyChanges(notification.TextDocument.Text);
             await Task.CompletedTask;
         }
 
         TextDocumentRegistrationOptions IRegistration<TextDocumentRegistrationOptions>.GetRegistrationOptions()
         {
-            return new TextDocumentRegistrationOptions() {
+            return new TextDocumentRegistrationOptions()
+            {
                 DocumentSelector = _documentSelector,
             };
         }
@@ -195,6 +123,7 @@ namespace SampleServer
         public Task Handle(DidCloseTextDocumentParams notification)
         {
             _router.LogMessage("DidCloseTextDocumentParams");
+            _ParseUnits.TryRemove(notification.TextDocument.Uri, out var  _);
             return Task.CompletedTask;
         }
 
@@ -206,7 +135,8 @@ namespace SampleServer
 
         TextDocumentSaveRegistrationOptions IRegistration<TextDocumentSaveRegistrationOptions>.GetRegistrationOptions()
         {
-            return new TextDocumentSaveRegistrationOptions() {
+            return new TextDocumentSaveRegistrationOptions()
+            {
                 DocumentSelector = _documentSelector,
                 IncludeText = Options.Save.IncludeText
             };
@@ -218,40 +148,20 @@ namespace SampleServer
             return new TextDocumentAttributes(uri, "plaintext");
         }
 
-#region "Hover"
+        #region "Hover"
         public Task<Hover> Handle(TextDocumentPositionParams request, CancellationToken token)
         {
-            if (parser == null || request == null)
-                return Task.FromResult<Hover>(null);
+            var pu = ParseUnitFor(request.TextDocument);
             _router.LogMessage("Hover over " + request.Position.Describe());
-            var alt = parser.LookingAt(request.Position);
-            return Task.FromResult(CreateHover(alt));
-        }
-
-        private Hover CreateHover(Alternative a)
-        {
-            if (a == null) return CreateHover(string.Empty);
-            return CreateHover(a.Describe(), a.t.ToRange());
-        }
-
-        private Hover CreateHover(string s)
-        {
-            return new Hover() { Contents = new MarkedStringContainer(new MarkedString(s)) };
-        }
-
-        private Hover CreateHover(IEnumerable<string> ss, Range r)
-        {
-            // see http://vshaxe.github.io/vscode-extern/vscode/MarkedString.html
-            _router.LogMessage("from " + r.Start.Describe() + "-" + r.End.Describe());
-            var qy = from s in ss select new MarkedString(s); // "markdown"
-            return new Hover() { Contents = new MarkedStringContainer(qy.ToArray()), Range = r };
+            var alt = pu.parser.LookingAt(request.Position);
+            return Task.FromResult(pu.CreateHover(alt));
         }
 
         public void SetCapability(HoverCapability capability)
         {
             _HoverCapability = capability;
         }
-# endregion
+        #endregion
     }
 
 }
